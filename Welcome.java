b51b0479b6dcb2c8951c37f90869dea36ebdd88f
desy4606 <138716284+desy4606@users.noreply.github.com>
2023-07-05 14:11:38 -0500

    Update and rename welcome.java.rtf to Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+public class welcome.java{
+  public static void main(Sting args[]) {
+    System.out.println("Welcome to java.");
+    System.out.println("Hello World");
+    
+    for(int i = 0; i < args.length; i++){
+      Systems.out.println(args[i]);
+    }
+  }
+}
