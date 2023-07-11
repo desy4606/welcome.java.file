9574d8c68857fd1212a934342ef58382dbf6ce10
desy4606 <138716284+desy4606@users.noreply.github.com>
2023-07-10 21:05:58 -0500

    Update CircleTester.java

--- CircleTester.java
+++ CircleTester.java
@@ -1,16 +1,14 @@
 public class CircleTester {
-    Circle c1 = new Circle(12.5, "blue");
+    Circle c1 = new Circle(4.0, "purple");
 
-        System.out.println("The color of the circle is " + c1.getColor());
+        System.out.println("The color of the circle is " + c1.getColor() );
 
          
-        //test accessing a private variable (q3)
+        //test for error message when trying to access a private variable 
         //System.out.println(c1.radius);
 
-        System.out.println("calling toString() explicitly:");
-	System.out.println(c1.toString());
-        System.out.println("\n calling toString() implicitly:");
-        System.out.println(c1);
+	System.out.println(c1.toString()); // explicitly calling toString();
+        System.out.println(c1); // implicitly calling toString();
 
         System.out.println("The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea());
 }
