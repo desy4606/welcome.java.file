4078bad35a7ac3ca3873561d1db17a354262d672
desy4606 <138716284+desy4606@users.noreply.github.com>
2023-07-10 19:26:33 -0500

    Update CircleTester.java

--- CircleTester.java
+++ CircleTester.java
@@ -4,6 +4,18 @@ public class CircleTester {
 
         System.out.println(c1);
 
+        // 3rd constructor to construct a new instance of Circle with the given radius and color
+        public Circle (double r, String c) {
+            Circle c2 = new Circle();
+
+            c1.radius = 0.5;
+            c2.r = 4;
+            c2.c = purple;
+        }
+
+        // Getter for instance variable color
+        public String getColor() {return color; }
+
         // System.out.println("The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea());
     }
 }
